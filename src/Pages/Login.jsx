0692ce70e4e78d87653139c4bc9327c46c9d8112
m_emin <mehmeteminbul@gmail.com>
2022-04-22 22:19:08 +0300

    assets added - login page ended

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import "./Login.css";
+import BlogIcon from "../assets/blok.png";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import googleLogo from "../assets/google.png";
+import { grey } from "@mui/material/colors";
 
-const Login = () => {
-  return <div>Login</div>;
-};
+export default function SimpleContainer() {
+  // const eventHandler = ()=>{
 
-export default Login;
+  // buraya email ve password bos birakildiginda error mesaji verilmesi fonksiyonu atanmali }
+
+  return (
+    <div className="login-container">
+      <React.Fragment>
+        <CssBaseline />
+        <Container maxWidth="sm">
+          <Box className="login-box">
+            <img className="blog-icon" src={BlogIcon} alt="blog_icon" />
+            <h2>── Login ──</h2>
+
+            <div className="login-textfields">
+              <TextField
+                type={"email"}
+                id="outlined-basic"
+                label="Email"
+                variant="outlined"
+                required
+              />
+
+              <TextField
+                type={"password"}
+                id="outlined-basic"
+                label="Password"
+                variant="outlined"
+                required
+              />
+            </div>
+            <div className="login-buttons">
+              {/* buttonlara hover ve background color eklenmeli */}
+              <Button variant="contained">LOGIN</Button>
+              <Button>
+                WITH
+                <span>
+                  <img
+                    className="google-logo"
+                    src={googleLogo}
+                    alt="google-logo"
+                  />
+                </span>
+              </Button>
+            </div>
+          </Box>
+        </Container>
+      </React.Fragment>
+    </div>
+  );
+}
