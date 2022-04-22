1169f1192190755b45f7d62a1e33178e9c5cc61f
m_emin <mehmeteminbul@gmail.com>
2022-04-22 22:45:38 +0300

    login and register pages added

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,14 +2,25 @@ import * as React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-import "./Login.css";
+import "./loginRegister.css";
 import BlogIcon from "../assets/blok.png";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import googleLogo from "../assets/google.png";
-import { grey } from "@mui/material/colors";
+// import { blueGrey } from "@mui/material/colors";
+// import { createTheme } from "@mui/material/styles";
 
 export default function SimpleContainer() {
+  // const theme = createTheme({
+  //   palette: {
+  //     primary: {
+  //       main: blueGrey[100],
+  //     },
+  //   },
+  // });
+
+  // const googleBtnColor = blueGrey[100];
+
   // const eventHandler = ()=>{
 
   // buraya email ve password bos birakildiginda error mesaji verilmesi fonksiyonu atanmali }
@@ -21,7 +32,7 @@ export default function SimpleContainer() {
         <Container maxWidth="sm">
           <Box className="login-box">
             <img className="blog-icon" src={BlogIcon} alt="blog_icon" />
-            <h2>── Login ──</h2>
+            <h2>── LOGIN ──</h2>
 
             <div className="login-textfields">
               <TextField
@@ -43,7 +54,15 @@ export default function SimpleContainer() {
             <div className="login-buttons">
               {/* buttonlara hover ve background color eklenmeli */}
               <Button variant="contained">LOGIN</Button>
-              <Button>
+              <Button
+              // sx={{
+              //   bgcolor: "main",
+              //   "&:hover": {
+              //     bgcolor: "primary.main",
+              //     color: "white",
+              //   },
+              // }} bu sekilde bg color degise biliyor renkleri nasil secilecek
+              >
                 WITH
                 <span>
                   <img
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import "./loginRegister.css";
+import BlogIcon from "../assets/blok.png";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import googleLogo from "../assets/google.png";
 
-const Register = () => {
-  return <div>Register</div>;
-};
+export default function SimpleContainer() {
+  // const eventHandler = ()=>{
 
-export default Register;
+  // buraya email ve password bos birakildiginda error mesaji verilmesi fonksiyonu atanmali }
+
+  return (
+    <div className="login-container">
+      <React.Fragment>
+        <CssBaseline />
+        <Container maxWidth="sm">
+          <Box className="login-box">
+            <img className="blog-icon" src={BlogIcon} alt="blog_icon" />
+            <h2>── REGISTER ──</h2>
+
+            <div className="login-textfields">
+              <TextField
+                type={"email"}
+                id="outlined-basic"
+                label="Email"
+                variant="outlined"
+                required
+              />
+
+              <TextField
+                type={"password"}
+                id="outlined-basic"
+                label="Password"
+                variant="outlined"
+                required
+              />
+            </div>
+            <div className="login-buttons">
+              {/* buttonlara hover ve background color eklenmeli */}
+              <Button variant="contained">REGISTER</Button>
+              <Button>
+                WITH
+                <span>
+                  <img
+                    className="google-logo"
+                    src={googleLogo}
+                    alt="google-logo"
+                  />
+                </span>
+              </Button>
+            </div>
+          </Box>
+        </Container>
+      </React.Fragment>
+    </div>
+  );
+}
