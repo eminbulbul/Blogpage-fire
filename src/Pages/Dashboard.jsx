8a815868f49feaecd1a64de40f7dbd214aae8bb3
m_emin <mehmeteminbul@gmail.com>
2022-04-25 03:09:32 +0300

    blog card dashboard and details pages updated

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import BlogCard from "../components/BlogCard";
 import "./Dashboard.css";
 
 const Dashboard = () => {
   return (
     <div>
       <h1 className="dash-text">──── Dashboard ────</h1>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "1rem",
+          // margin: "3rem",
+          justifyContent: "center",
+        }}
+      >
+        <BlogCard style={{ margin: "auto" }} />
+        <BlogCard style={{ margin: "auto" }} />
+      </div>
     </div>
   );
 };
